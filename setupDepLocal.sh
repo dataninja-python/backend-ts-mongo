9e685e73ea6a4b3df7d42f620d853fa5caa30d92
dataninja-python <aj@redstonema.com>
2022-09-05 11:51:46 -0400

    updated project

--- setupDepLocal.sh
+++ setupDepLocal.sh
@@ -13,6 +13,14 @@ pnpm add mongoose
 pnpm add -D typescript
 pnpm add -D @types/express
 
+# aeshtetics && experience
+# make sure following installed in vscode (if using it):
+# DotENV
+# Prettier
+# vscode-icons
+
+
+
 # update/upgrade
 echo "NOTE: how should I update packages?"
 read -p "Enter 'y' to update to latest package versions regardless of recommendations. What do you want me to do:  " ans
@@ -20,7 +28,9 @@ read -p "Enter 'y' to update to latest package versions regardless of recommenda
 if [ $ans == "y" ]; then
     echo "Success! Installing packages to latest versions."
     pnpm up --latest
+    tsc --init
     exit 0
 fi
 
+tsc --init
 pnpm up
